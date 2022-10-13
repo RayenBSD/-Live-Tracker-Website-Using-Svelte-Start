f5337cf2bee52b771ff789b3c14ba308e4a8599b
RayenBSD <92104474+RayenBSD@users.noreply.github.com>
2022-10-13 12:07:40 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Areas from "./pages/Areas.svelte";
+    import Blog from "./pages/Blog.svelte";
+    import Cards from "./pages/Cards.svelte";
+    import Footer from "./pages/Footer.svelte";
+    import Header from "./pages/Header.svelte";
+</script>
+
+<main>
+    <Header />
+    <Areas />
+    <Cards />
+    <Cards />
+    <Blog />
+    <Footer />
+</main>
+
+<style>
+
+</style>
